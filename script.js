537f0b27cdd1d0ec9c919e50b3bcea15561598d2
KATO Jinnosuke <23ec032@ms.dendai.ac.jp>
2025-12-23 11:49:34 +0900

    行番号付与ロジックを簡単なものにした

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 document.getElementById("input").addEventListener("input", (event) => {
-  let row = 1;
-  const result = event.target.value
-    .split("")
-    .reduce((accumulator, char) => {
-      if (char === "\n"){
-        row += 1;
-        return accumulator + char + `${row} `;
-      }
-      else
-        return accumulator + char;
-    }, row + " ");
-
-    document.getElementById("result").value = result;
+  document.getElementById("result").value = event.target.value
+  .split("\n")
+  .map((line, index) => `${index + 1} ${line}`)
+  .join("\n");
 });
